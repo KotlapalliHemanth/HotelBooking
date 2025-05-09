9495282a8e86e59bf3a94e8b1ec8865b303ec873
KotlapalliHemanth <kotlapallihemanth.k97@gmail.com>
2025-05-09 17:01:24 +0530

    initial set up

---
+++ src/test/java/com/kotlapalli/Hotelbooking/HotelbookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kotlapalli.Hotelbooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelbookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
